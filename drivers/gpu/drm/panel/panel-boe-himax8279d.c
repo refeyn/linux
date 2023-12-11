0dd6a90e808c99c03430eee545d72739506dcf25
Matthew Joyce <matthew.joyce@refeyn.com>
2023-12-11 15:46:42 +0000

    VERY UGLY touchscreen driver hacks

--- drivers/gpu/drm/panel/panel-boe-himax8279d.c
+++ drivers/gpu/drm/panel/panel-boe-himax8279d.c
@@ -21,7 +21,7 @@
 #include <drm/drm_panel.h>
 
 #include <video/mipi_display.h>
-
+#define DEBUG
 struct panel_cmd {
 	char cmd;
 	char data;
@@ -45,12 +45,14 @@ struct panel_info {
 	struct mipi_dsi_device *link;
 	const struct panel_desc *desc;
 
-	struct gpio_desc *enable_gpio;
-	struct gpio_desc *pp33_gpio;
-	struct gpio_desc *pp18_gpio;
+	// struct gpio_desc *enable_gpio;
+	// struct gpio_desc *pp33_gpio;
+	// struct gpio_desc *pp18_gpio;
 
 	bool prepared;
 	bool enabled;
+
+	enum drm_panel_orientation orientation;
 };
 
 static inline struct panel_info *to_panel_info(struct drm_panel *panel)
@@ -60,9 +62,9 @@ static inline struct panel_info *to_panel_info(struct drm_panel *panel)
 
 static void disable_gpios(struct panel_info *pinfo)
 {
-	gpiod_set_value(pinfo->enable_gpio, 0);
-	gpiod_set_value(pinfo->pp33_gpio, 0);
-	gpiod_set_value(pinfo->pp18_gpio, 0);
+	// gpiod_set_value(pinfo->enable_gpio, 0);
+	// gpiod_set_value(pinfo->pp33_gpio, 0);
+	// gpiod_set_value(pinfo->pp18_gpio, 0);
 }
 
 static int send_mipi_cmds(struct drm_panel *panel, const struct panel_cmd *cmds)
@@ -70,6 +72,8 @@ static int send_mipi_cmds(struct drm_panel *panel, const struct panel_cmd *cmds)
 	struct panel_info *pinfo = to_panel_info(panel);
 	unsigned int i = 0;
 	int err;
+	ssize_t len;
+	char buf[2];
 
 	for (i = 0; i < pinfo->desc->on_cmds_num; i++) {
 		err = mipi_dsi_dcs_write_buffer(pinfo->link, &cmds[i],
@@ -79,13 +83,48 @@ static int send_mipi_cmds(struct drm_panel *panel, const struct panel_cmd *cmds)
 			return err;
 	}
 
+
+	len = mipi_dsi_dcs_read(pinfo->link, 0xff, &buf, 2);
+	pr_alert("BOE DCS %zd %d %d send %u cmds", len, (int)buf[0], (int)buf[1], pinfo->desc->on_cmds_num);
+
 	return 0;
 }
+
+// void read_dcs(struct drm_panel *panel, char page, u8 reg) {
+// 	struct panel_info *pinfo = to_panel_info(panel);
+// 	char buf[2];
+// 	ssize_t len;
+// 	int err;
+// 	char cmds[2] = {0xB0, page};
+// 	err = mipi_dsi_dcs_write_buffer(pinfo->link, &cmds, sizeof(struct panel_cmd));
+// 	if (err < 0) {
+// 		pr_alert("BOE DCS FAILED %d", err);
+// 	}
+// 	else {
+// 		len = mipi_dsi_dcs_read(pinfo->link, reg, &buf, 2);
+// 		pr_alert("BOE DCS [%d:%d] %zd %d %d", (int)page, (int)reg, len, (int)buf[0], (int)buf[1]);
+// 	}
+// }
+//
+// void write_dcs(struct drm_panel *panel, char page, char reg, char val) {
+// 	struct panel_info *pinfo = to_panel_info(panel);
+// 	int err;
+// 	char cmds[4] = {0xB0, page, reg, val};
+// 	err = mipi_dsi_dcs_write_buffer(pinfo->link, &cmds, sizeof(struct panel_cmd));
+// 	if (err < 0) {
+// 		pr_alert("BOE DCSW FAILED %d", err);
+// 	}
+// 	err = mipi_dsi_dcs_write_buffer(pinfo->link, &cmds[2], sizeof(struct panel_cmd));
+// 	if (err < 0) {
+// 		pr_alert("BOE DCSW2 FAILED %d", err);
+// 	}
+// }
 
 static int boe_panel_disable(struct drm_panel *panel)
 {
 	struct panel_info *pinfo = to_panel_info(panel);
 	int err;
+	pr_alert("BOE DISABLE\n");
 
 	if (!pinfo->enabled)
 		return 0;
@@ -105,6 +144,7 @@ static int boe_panel_unprepare(struct drm_panel *panel)
 {
 	struct panel_info *pinfo = to_panel_info(panel);
 	int err;
+	pr_alert("BOE UNPREPARE\n");
 
 	if (!pinfo->prepared)
 		return 0;
@@ -134,52 +174,28 @@ static int boe_panel_prepare(struct drm_panel *panel)
 
 	if (pinfo->prepared)
 		return 0;
+	pr_alert("BOE PREPARE\n");
 
-	gpiod_set_value(pinfo->pp18_gpio, 1);
-	/* T1: 5ms - 6ms */
-	usleep_range(5000, 6000);
-	gpiod_set_value(pinfo->pp33_gpio, 1);
-
-	/* reset sequence */
-	/* T2: 14ms - 15ms */
-	usleep_range(14000, 15000);
-	gpiod_set_value(pinfo->enable_gpio, 1);
-
-	/* T3: 1ms - 2ms */
-	usleep_range(1000, 2000);
-	gpiod_set_value(pinfo->enable_gpio, 0);
-
-	/* T4: 1ms - 2ms */
-	usleep_range(1000, 2000);
-	gpiod_set_value(pinfo->enable_gpio, 1);
-
-	/* T5: 5ms - 6ms */
-	usleep_range(5000, 6000);
-
-	/* send init code */
-	err = send_mipi_cmds(panel, pinfo->desc->on_cmds);
-	if (err < 0) {
-		dev_err(panel->dev, "failed to send DCS Init Code: %d\n", err);
-		goto poweroff;
-	}
-
-	err = mipi_dsi_dcs_exit_sleep_mode(pinfo->link);
-	if (err < 0) {
-		dev_err(panel->dev, "failed to exit sleep mode: %d\n", err);
-		goto poweroff;
-	}
-
-	/* T6: 120ms - 121ms */
-	usleep_range(120000, 121000);
-
-	err = mipi_dsi_dcs_set_display_on(pinfo->link);
-	if (err < 0) {
-		dev_err(panel->dev, "failed to set display on: %d\n", err);
-		goto poweroff;
-	}
-
-	/* T7: 20ms - 21ms */
-	usleep_range(20000, 21000);
+	// gpiod_set_value(pinfo->pp18_gpio, 1);
+	// /* T1: 5ms - 6ms */
+	// usleep_range(5000, 6000);
+	// gpiod_set_value(pinfo->pp33_gpio, 1);
+ //
+	// /* reset sequence */
+	// /* T2: 14ms - 15ms */
+	// usleep_range(14000, 15000);
+	// gpiod_set_value(pinfo->enable_gpio, 1);
+ //
+	// /* T3: 1ms - 2ms */
+	// usleep_range(1000, 2000);
+	// gpiod_set_value(pinfo->enable_gpio, 0);
+ //
+	// /* T4: 1ms - 2ms */
+	// usleep_range(1000, 2000);
+	// gpiod_set_value(pinfo->enable_gpio, 1);
+ //
+	// /* T5: 5ms - 6ms */
+	// usleep_range(5000, 6000);
 
 	pinfo->prepared = true;
 
@@ -193,18 +209,45 @@ poweroff:
 static int boe_panel_enable(struct drm_panel *panel)
 {
 	struct panel_info *pinfo = to_panel_info(panel);
-	int ret;
+	int err;
 
 	if (pinfo->enabled)
 		return 0;
 
+	// Do reset
+	err = mipi_dsi_dcs_soft_reset(pinfo->link);
+	if (err < 0) {
+		dev_err(panel->dev, "failed to send soft reset: %d\n", err);
+		return err;
+	}
+
+	usleep_range(5000, 6000);
+	pr_alert("BOE ENABLE\n");
+
+	/* send init code */
+	err = send_mipi_cmds(panel, pinfo->desc->on_cmds);
+	if (err < 0) {
+		dev_err(panel->dev, "failed to send DCS Init Code: %d\n", err);
+		return err;
+	}
+
+	usleep_range(200000, 210000);
+
+	err = mipi_dsi_dcs_exit_sleep_mode(pinfo->link);
+	if (err < 0) {
+		dev_err(panel->dev, "failed to exit sleep mode: %d\n", err);
+		return err;
+	}
+
+	/* T6: 120ms - 121ms */
 	usleep_range(120000, 121000);
 
-	ret = mipi_dsi_dcs_set_display_on(pinfo->link);
-	if (ret < 0) {
-		dev_err(panel->dev, "failed to set display on: %d\n", ret);
-		return ret;
+	err = mipi_dsi_dcs_set_display_on(pinfo->link);
+	if (err < 0) {
+		dev_err(panel->dev, "failed to set display on: %d\n", err);
+		return err;
 	}
+	usleep_range(50000, 51000);
 
 	pinfo->enabled = true;
 
@@ -225,6 +268,8 @@ static int boe_panel_get_modes(struct drm_panel *panel,
 		return -ENOMEM;
 	}
 
+	pr_alert("BOE DISP MODE %u %d", mode->clock, (int) pinfo->orientation);
+
 	drm_mode_set_name(mode);
 
 	drm_mode_probed_add(connector, mode);
@@ -233,7 +278,22 @@ static int boe_panel_get_modes(struct drm_panel *panel,
 	connector->display_info.height_mm = pinfo->desc->height_mm;
 	connector->display_info.bpc = pinfo->desc->bpc;
 
+	/*
+	 * TODO: Remove once all drm drivers call
+	 * drm_connector_set_orientation_from_panel()
+	 */
+	drm_connector_set_panel_orientation(connector, pinfo->orientation);
+
 	return 1;
+}
+
+static enum drm_panel_orientation boe_panel_get_orientation(struct drm_panel *panel)
+{
+	pr_alert("BOE ORIENTATION");
+
+	struct panel_info *pinfo = to_panel_info(panel);
+
+	return pinfo->orientation;
 }
 
 static const struct drm_panel_funcs panel_funcs = {
@@ -242,18 +302,39 @@ static const struct drm_panel_funcs panel_funcs = {
 	.prepare = boe_panel_prepare,
 	.enable = boe_panel_enable,
 	.get_modes = boe_panel_get_modes,
+	.get_orientation = boe_panel_get_orientation,
 };
 
 static const struct drm_display_mode default_display_mode = {
-	.clock = 159420,
+	// .clock = 159420,
+	// .hdisplay = 1200,
+	// .hsync_start = 1200 + 80,
+	// .hsync_end = 1200 + 80 + 60,
+	// .htotal = 1200 + 80 + 60 + 24,
+	// .vdisplay = 1920,
+	// .vsync_start = 1920 + 10,
+	// .vsync_end = 1920 + 10 + 14,
+	// .vtotal = 1920 + 10 + 14 + 4,
+
+	.clock = 20000,
 	.hdisplay = 1200,
-	.hsync_start = 1200 + 80,
-	.hsync_end = 1200 + 80 + 60,
-	.htotal = 1200 + 80 + 60 + 24,
+	.hsync_start = 1200 + 40,
+	.hsync_end = 1200 + 40 + 10,
+	.htotal = 1200 + 40 + 10 + 50,
 	.vdisplay = 1920,
 	.vsync_start = 1920 + 10,
-	.vsync_end = 1920 + 10 + 14,
-	.vtotal = 1920 + 10 + 14 + 4,
+	.vsync_end = 1920 + 10 + 10,
+	.vtotal = 1920 + 10 + 10 + 20,
+//
+	// .clock = 156800,
+	// .hdisplay = 1200,
+	// .hsync_start = 1200 + 110,
+	// .hsync_end = 1200 + 110 + 1,
+	// .htotal = 1200 + 110 + 1 + 32,
+	// .vdisplay = 1920,
+	// .vsync_start = 1920 + 11,
+	// .vsync_end = 1920 + 11 + 1,
+	// .vtotal = 1920 + 11 + 1 + 14,
 };
 
 /* 8 inch */
@@ -535,289 +616,95 @@ static const struct panel_desc boe_himax8279d8p_panel_desc = {
 
 /* 10 inch */
 static const struct panel_cmd boe_himax8279d10p_on_cmds[] = {
-	{ 0xB0, 0x05 },
-	{ 0xB1, 0xE5 },
-	{ 0xB3, 0x52 },
-	{ 0xB0, 0x00 },
-	{ 0xB6, 0x03 },
-	{ 0xBA, 0x8B },
-	{ 0xBF, 0x1A },
-	{ 0xC0, 0x0F },
-	{ 0xC2, 0x0C },
-	{ 0xC3, 0x02 },
-	{ 0xC4, 0x0C },
-	{ 0xC5, 0x02 },
-	{ 0xB0, 0x01 },
-	{ 0xE0, 0x26 },
-	{ 0xE1, 0x26 },
-	{ 0xDC, 0x00 },
-	{ 0xDD, 0x00 },
-	{ 0xCC, 0x26 },
-	{ 0xCD, 0x26 },
-	{ 0xC8, 0x00 },
-	{ 0xC9, 0x00 },
-	{ 0xD2, 0x03 },
-	{ 0xD3, 0x03 },
-	{ 0xE6, 0x04 },
-	{ 0xE7, 0x04 },
-	{ 0xC4, 0x09 },
-	{ 0xC5, 0x09 },
-	{ 0xD8, 0x0A },
-	{ 0xD9, 0x0A },
-	{ 0xC2, 0x0B },
-	{ 0xC3, 0x0B },
-	{ 0xD6, 0x0C },
-	{ 0xD7, 0x0C },
-	{ 0xC0, 0x05 },
-	{ 0xC1, 0x05 },
-	{ 0xD4, 0x06 },
-	{ 0xD5, 0x06 },
-	{ 0xCA, 0x07 },
-	{ 0xCB, 0x07 },
-	{ 0xDE, 0x08 },
-	{ 0xDF, 0x08 },
-	{ 0xB0, 0x02 },
+	{ 0xB0, 0x01 },//PAGE 1 (OTP & GOA MUX)
+	{ 0xC3, 0x0F },
+	{ 0xC4, 0x00 },
+	{ 0xC5, 0x00 },
+	{ 0xC6, 0x00 },
+	{ 0xC7, 0x00 },
+	{ 0xC8, 0x0D },
+	{ 0xC9, 0x12 },
+	{ 0xCA, 0x11 },
+	{ 0xCD, 0x1D },
+	{ 0xCE, 0x1B },
+	{ 0xCF, 0x0B },
+	{ 0xD0, 0x09 },//gap
+	{ 0xD1, 0x07 },
+	{ 0xD2, 0x05 },
+	{ 0xD3, 0x01 },
+	{ 0xD7, 0x10 },
+	{ 0xD8, 0x00 },
+	{ 0xD9, 0x00 },
+	{ 0xDA, 0x00 },
+	{ 0xDB, 0x00 },
+	{ 0xDC, 0x0E },
+	{ 0xDD, 0x12 },
+	{ 0xDE, 0x11 },
+	{ 0xE1, 0x1E },//gap
+	{ 0xE2, 0x1C },
+	{ 0xE3, 0x0C },
+	{ 0xE4, 0x0A },
+	{ 0xE5, 0x08 },
+	{ 0xE6, 0x06 },
+	{ 0xE7, 0x02 },
+	{ 0xB0, 0x03 },//PAGE 3 (GOA)
+	{ 0xBE, 0x03 },
+	{ 0xCC, 0x44 },
+	{ 0xC8, 0x07 },
+	{ 0xC9, 0x05 },
+	{ 0xCA, 0x42 },
+	{ 0xCD, 0x3E },
+	{ 0xCF, 0x60 },
+	{ 0xD2, 0x04 },
+	{ 0xD3, 0x04 },
+	{ 0xD4, 0x01 },
+	{ 0xD5, 0x01 },
+	{ 0xD6, 0x03 },
+	{ 0xD7, 0x04 },
+	{ 0xD9, 0x01 },
+	{ 0xDB, 0x01 },
+	{ 0xE4, 0xF0 },
+	{ 0xE5, 0x0A },
+	{ 0xB0, 0x00 },//PAGE 0
+	{ 0xBD, 0x63 },
+	{ 0xC2, 0x08 },
+	{ 0xC4, 0x10 },
+	{ 0xB0, 0x02 },//PAGE 2 (analog gamma)
 	{ 0xC0, 0x00 },
-	{ 0xC1, 0x0D },
-	{ 0xC2, 0x17 },
-	{ 0xC3, 0x26 },
-	{ 0xC4, 0x31 },
-	{ 0xC5, 0x1C },
-	{ 0xC6, 0x2C },
-	{ 0xC7, 0x33 },
-	{ 0xC8, 0x31 },
-	{ 0xC9, 0x37 },
-	{ 0xCA, 0x37 },
-	{ 0xCB, 0x37 },
-	{ 0xCC, 0x39 },
-	{ 0xCD, 0x2E },
-	{ 0xCE, 0x2F },
-	{ 0xCF, 0x2F },
-	{ 0xD0, 0x07 },
+	{ 0xC1, 0x0A },
+	{ 0xC2, 0x20 },
+	{ 0xC3, 0x24 },
+	{ 0xC4, 0x23 },
+	{ 0xC5, 0x29 },
+	{ 0xC6, 0x23 },
+	{ 0xC7, 0x1C },
+	{ 0xC8, 0x19 },
+	{ 0xC9, 0x17 },
+	{ 0xCA, 0x17 },
+	{ 0xCB, 0x18 },
+	{ 0xCC, 0x1A },
+	{ 0xCD, 0x1E },
+	{ 0xCE, 0x20 },
+	{ 0xCF, 0x23 },
+	{ 0xD0, 0x07 },//gap
+	{ 0xD1, 0x00 },
 	{ 0xD2, 0x00 },
-	{ 0xD3, 0x0D },
-	{ 0xD4, 0x17 },
-	{ 0xD5, 0x26 },
-	{ 0xD6, 0x31 },
-	{ 0xD7, 0x3F },
-	{ 0xD8, 0x3F },
-	{ 0xD9, 0x3F },
-	{ 0xDA, 0x3F },
-	{ 0xDB, 0x37 },
-	{ 0xDC, 0x37 },
-	{ 0xDD, 0x37 },
-	{ 0xDE, 0x39 },
-	{ 0xDF, 0x2E },
-	{ 0xE0, 0x2F },
-	{ 0xE1, 0x2F },
+	{ 0xD3, 0x0A },
+	{ 0xD4, 0x05 },
+	{ 0xD5, 0x1C },
+	{ 0xD6, 0x1A },
+	{ 0xD7, 0x13 },
+	{ 0xD8, 0x17 },
+	{ 0xD9, 0x1C },
+	{ 0xDA, 0x19 },
+	{ 0xDB, 0x17 },
+	{ 0xDC, 0x17 },
+	{ 0xDD, 0x18 },
+	{ 0xDE, 0x1A },
+	{ 0xDF, 0x1E },
+	{ 0xE0, 0x20 },
+	{ 0xE1, 0x23 },
 	{ 0xE2, 0x07 },
-	{ 0xB0, 0x03 },
-	{ 0xC8, 0x0B },
-	{ 0xC9, 0x07 },
-	{ 0xC3, 0x00 },
-	{ 0xE7, 0x00 },
-	{ 0xC5, 0x2A },
-	{ 0xDE, 0x2A },
-	{ 0xCA, 0x43 },
-	{ 0xC9, 0x07 },
-	{ 0xE4, 0xC0 },
-	{ 0xE5, 0x0D },
-	{ 0xCB, 0x01 },
-	{ 0xBC, 0x01 },
-	{ 0xB0, 0x06 },
-	{ 0xB8, 0xA5 },
-	{ 0xC0, 0xA5 },
-	{ 0xC7, 0x0F },
-	{ 0xD5, 0x32 },
-	{ 0xB8, 0x00 },
-	{ 0xC0, 0x00 },
-	{ 0xBC, 0x00 },
-	{ 0xB0, 0x07 },
-	{ 0xB1, 0x00 },
-	{ 0xB2, 0x05 },
-	{ 0xB3, 0x10 },
-	{ 0xB4, 0x22 },
-	{ 0xB5, 0x36 },
-	{ 0xB6, 0x4A },
-	{ 0xB7, 0x6C },
-	{ 0xB8, 0x9A },
-	{ 0xB9, 0xD7 },
-	{ 0xBA, 0x17 },
-	{ 0xBB, 0x92 },
-	{ 0xBC, 0x15 },
-	{ 0xBD, 0x18 },
-	{ 0xBE, 0x8C },
-	{ 0xBF, 0x00 },
-	{ 0xC0, 0x3A },
-	{ 0xC1, 0x72 },
-	{ 0xC2, 0x8C },
-	{ 0xC3, 0xA5 },
-	{ 0xC4, 0xB1 },
-	{ 0xC5, 0xBE },
-	{ 0xC6, 0xCA },
-	{ 0xC7, 0xD1 },
-	{ 0xC8, 0xD4 },
-	{ 0xC9, 0x00 },
-	{ 0xCA, 0x00 },
-	{ 0xCB, 0x16 },
-	{ 0xCC, 0xAF },
-	{ 0xCD, 0xFF },
-	{ 0xCE, 0xFF },
-	{ 0xB0, 0x08 },
-	{ 0xB1, 0x04 },
-	{ 0xB2, 0x05 },
-	{ 0xB3, 0x11 },
-	{ 0xB4, 0x24 },
-	{ 0xB5, 0x39 },
-	{ 0xB6, 0x4E },
-	{ 0xB7, 0x72 },
-	{ 0xB8, 0xA3 },
-	{ 0xB9, 0xE1 },
-	{ 0xBA, 0x25 },
-	{ 0xBB, 0xA8 },
-	{ 0xBC, 0x2E },
-	{ 0xBD, 0x32 },
-	{ 0xBE, 0xAD },
-	{ 0xBF, 0x28 },
-	{ 0xC0, 0x63 },
-	{ 0xC1, 0x9B },
-	{ 0xC2, 0xB5 },
-	{ 0xC3, 0xCF },
-	{ 0xC4, 0xDB },
-	{ 0xC5, 0xE8 },
-	{ 0xC6, 0xF5 },
-	{ 0xC7, 0xFA },
-	{ 0xC8, 0xFC },
-	{ 0xC9, 0x00 },
-	{ 0xCA, 0x00 },
-	{ 0xCB, 0x16 },
-	{ 0xCC, 0xAF },
-	{ 0xCD, 0xFF },
-	{ 0xCE, 0xFF },
-	{ 0xB0, 0x09 },
-	{ 0xB1, 0x04 },
-	{ 0xB2, 0x04 },
-	{ 0xB3, 0x0F },
-	{ 0xB4, 0x22 },
-	{ 0xB5, 0x37 },
-	{ 0xB6, 0x4D },
-	{ 0xB7, 0x71 },
-	{ 0xB8, 0xA2 },
-	{ 0xB9, 0xE1 },
-	{ 0xBA, 0x26 },
-	{ 0xBB, 0xA9 },
-	{ 0xBC, 0x2F },
-	{ 0xBD, 0x33 },
-	{ 0xBE, 0xAC },
-	{ 0xBF, 0x24 },
-	{ 0xC0, 0x5D },
-	{ 0xC1, 0x94 },
-	{ 0xC2, 0xAC },
-	{ 0xC3, 0xC5 },
-	{ 0xC4, 0xD1 },
-	{ 0xC5, 0xDC },
-	{ 0xC6, 0xE8 },
-	{ 0xC7, 0xED },
-	{ 0xC8, 0xF0 },
-	{ 0xC9, 0x00 },
-	{ 0xCA, 0x00 },
-	{ 0xCB, 0x16 },
-	{ 0xCC, 0xAF },
-	{ 0xCD, 0xFF },
-	{ 0xCE, 0xFF },
-	{ 0xB0, 0x0A },
-	{ 0xB1, 0x00 },
-	{ 0xB2, 0x05 },
-	{ 0xB3, 0x10 },
-	{ 0xB4, 0x22 },
-	{ 0xB5, 0x36 },
-	{ 0xB6, 0x4A },
-	{ 0xB7, 0x6C },
-	{ 0xB8, 0x9A },
-	{ 0xB9, 0xD7 },
-	{ 0xBA, 0x17 },
-	{ 0xBB, 0x92 },
-	{ 0xBC, 0x15 },
-	{ 0xBD, 0x18 },
-	{ 0xBE, 0x8C },
-	{ 0xBF, 0x00 },
-	{ 0xC0, 0x3A },
-	{ 0xC1, 0x72 },
-	{ 0xC2, 0x8C },
-	{ 0xC3, 0xA5 },
-	{ 0xC4, 0xB1 },
-	{ 0xC5, 0xBE },
-	{ 0xC6, 0xCA },
-	{ 0xC7, 0xD1 },
-	{ 0xC8, 0xD4 },
-	{ 0xC9, 0x00 },
-	{ 0xCA, 0x00 },
-	{ 0xCB, 0x16 },
-	{ 0xCC, 0xAF },
-	{ 0xCD, 0xFF },
-	{ 0xCE, 0xFF },
-	{ 0xB0, 0x0B },
-	{ 0xB1, 0x04 },
-	{ 0xB2, 0x05 },
-	{ 0xB3, 0x11 },
-	{ 0xB4, 0x24 },
-	{ 0xB5, 0x39 },
-	{ 0xB6, 0x4E },
-	{ 0xB7, 0x72 },
-	{ 0xB8, 0xA3 },
-	{ 0xB9, 0xE1 },
-	{ 0xBA, 0x25 },
-	{ 0xBB, 0xA8 },
-	{ 0xBC, 0x2E },
-	{ 0xBD, 0x32 },
-	{ 0xBE, 0xAD },
-	{ 0xBF, 0x28 },
-	{ 0xC0, 0x63 },
-	{ 0xC1, 0x9B },
-	{ 0xC2, 0xB5 },
-	{ 0xC3, 0xCF },
-	{ 0xC4, 0xDB },
-	{ 0xC5, 0xE8 },
-	{ 0xC6, 0xF5 },
-	{ 0xC7, 0xFA },
-	{ 0xC8, 0xFC },
-	{ 0xC9, 0x00 },
-	{ 0xCA, 0x00 },
-	{ 0xCB, 0x16 },
-	{ 0xCC, 0xAF },
-	{ 0xCD, 0xFF },
-	{ 0xCE, 0xFF },
-	{ 0xB0, 0x0C },
-	{ 0xB1, 0x04 },
-	{ 0xB2, 0x04 },
-	{ 0xB3, 0x0F },
-	{ 0xB4, 0x22 },
-	{ 0xB5, 0x37 },
-	{ 0xB6, 0x4D },
-	{ 0xB7, 0x71 },
-	{ 0xB8, 0xA2 },
-	{ 0xB9, 0xE1 },
-	{ 0xBA, 0x26 },
-	{ 0xBB, 0xA9 },
-	{ 0xBC, 0x2F },
-	{ 0xBD, 0x33 },
-	{ 0xBE, 0xAC },
-	{ 0xBF, 0x24 },
-	{ 0xC0, 0x5D },
-	{ 0xC1, 0x94 },
-	{ 0xC2, 0xAC },
-	{ 0xC3, 0xC5 },
-	{ 0xC4, 0xD1 },
-	{ 0xC5, 0xDC },
-	{ 0xC6, 0xE8 },
-	{ 0xC7, 0xED },
-	{ 0xC8, 0xF0 },
-	{ 0xC9, 0x00 },
-	{ 0xCA, 0x00 },
-	{ 0xCB, 0x16 },
-	{ 0xCC, 0xAF },
-	{ 0xCD, 0xFF },
-	{ 0xCE, 0xFF },
 };
 
 static const struct panel_desc boe_himax8279d10p_panel_desc = {
@@ -830,7 +717,7 @@ static const struct panel_desc boe_himax8279d10p_panel_desc = {
 	.format = MIPI_DSI_FMT_RGB888,
 	.lanes = 4,
 	.on_cmds = boe_himax8279d10p_on_cmds,
-	.on_cmds_num = 283,
+	.on_cmds_num = 89,
 };
 
 static const struct of_device_id panel_of_match[] = {
@@ -852,7 +739,7 @@ static int panel_add(struct panel_info *pinfo)
 {
 	struct device *dev = &pinfo->link->dev;
 	int ret;
-
+/*
 	pinfo->pp18_gpio = devm_gpiod_get(dev, "pp18", GPIOD_OUT_HIGH);
 	if (IS_ERR(pinfo->pp18_gpio)) {
 		ret = PTR_ERR(pinfo->pp18_gpio);
@@ -875,8 +762,25 @@ static int panel_add(struct panel_info *pinfo)
 		if (ret != -EPROBE_DEFER)
 			dev_err(dev, "failed to get enable gpio: %d\n", ret);
 		return ret;
-	}
+	}*/
 
+	ret = of_drm_get_panel_orientation(dev->of_node, &pinfo->orientation);
+	if (ret) {
+		dev_err(dev, "%pOF: failed to get orientation %d\n", dev->of_node, ret);
+		return ret;
+	}
+	pinfo->orientation = DRM_MODE_PANEL_ORIENTATION_BOTTOM_UP;
+
+
+
+	// ret = of_get_drm_panel_display_mode(np, &lvds->dmode, &lvds->bus_flags);
+	// if (ret < 0) {
+	// 	dev_err(lvds->dev, "%pOF: problems parsing panel-timing (%d)\n",
+	// 		np, ret);
+	// 	return ret;
+	// }
+
+	pr_alert("BOE ADD\n");
 	drm_panel_init(&pinfo->base, dev, &panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
 
@@ -885,6 +789,7 @@ static int panel_add(struct panel_info *pinfo)
 		return ret;
 
 	drm_panel_add(&pinfo->base);
+	pr_alert("BOE ADD END\n");
 
 	return 0;
 }
@@ -899,6 +804,7 @@ static int panel_probe(struct mipi_dsi_device *dsi)
 	if (!pinfo)
 		return -ENOMEM;
 
+	pr_alert("BOE PROBE\n");
 	desc = of_device_get_match_data(&dsi->dev);
 	dsi->mode_flags = desc->mode_flags;
 	dsi->format = desc->format;
@@ -909,12 +815,17 @@ static int panel_probe(struct mipi_dsi_device *dsi)
 	mipi_dsi_set_drvdata(dsi, pinfo);
 
 	err = panel_add(pinfo);
-	if (err < 0)
+	if (err < 0) {
+		pr_alert("BOE PROBE: err in add %d\n", err);
 		return err;
+	}
 
 	err = mipi_dsi_attach(dsi);
-	if (err < 0)
+	if (err < 0) {
+		pr_alert("BOE PROBE: err in attach %d\n", err);
 		drm_panel_remove(&pinfo->base);
+	}
+	pr_alert("BOE PROBE END\n");
 
 	return err;
 }
