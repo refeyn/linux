cf6c0df1f8adf315350256da5e676de467765027
Wen Gong <quic_wgong@quicinc.com>
2023-12-17 13:29:03 +0200

    wifi: ath12k: add string type to search board data in board-2.bin for WCN7850
    Currently ath12k only supports string type with bus, chip id and board id
    such as "bus=ahb,qmi-chip-id=1,qmi-board-id=4" for AHB bus chip and
    "bus=pci,qmi-chip-id=0,qmi-board-id=255" for PCI bus chip in board-2.bin.
    For WCN7850, it is not enough to distinguish all different chips.
    
    Add a new string type which includes bus, chip id, board id, vendor,
    device, subsystem-vendor and subsystem-device for WCN7850.
    
    ath12k will first load board-2.bin and searches in it for the board data
    with the above parameters. If matched with one board data, ath12k downloads
    it to firmware. And if not, downloads board.bin instead.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0-03427-QCAHMTSWPL_V1.0_V2.0_SILICONZ-1.15378.4
    
    Upstream-Status: Backport [f4c2a9d62213cf7004db5c74ce12d26d63a23629]
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Baochen Qiang <quic_bqiang@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://msgid.link/20231216060140.30611-2-quic_bqiang@quicinc.com

--- drivers/net/wireless/ath/ath12k/core.h
+++ drivers/net/wireless/ath/ath12k/core.h
@@ -44,6 +44,11 @@
 #define ATH12K_RECONFIGURE_TIMEOUT_HZ		(10 * HZ)
 #define ATH12K_RECOVER_START_TIMEOUT_HZ		(20 * HZ)
 
+enum ath12k_bdf_search {
+	ATH12K_BDF_SEARCH_DEFAULT,
+	ATH12K_BDF_SEARCH_BUS_AND_BOARD,
+};
+
 enum wme_ac {
 	WME_AC_BE,
 	WME_AC_BK,
@@ -773,6 +778,14 @@ struct ath12k_base {
 
 	u64 fw_soc_drop_count;
 	bool static_window_map;
+
+	struct {
+		enum ath12k_bdf_search bdf_search;
+		u32 vendor;
+		u32 device;
+		u32 subsystem_vendor;
+		u32 subsystem_device;
+	} id;
 
 	/* must be last */
 	u8 drv_priv[] __aligned(sizeof(void *));
