c4658e4c4671b6bd481431cf0dd1d2e774b94f32
Matthew Joyce <matthew.joyce@refeyn.com>
2025-05-19 11:00:34 +0100

    drm/panel: densitron-himax8279d: Make clock continuous, as the AM69 driver doesn't seem to work otherwise

--- drivers/gpu/drm/panel/panel-densitron-himax8279d.c
+++ drivers/gpu/drm/panel/panel-densitron-himax8279d.c
@@ -292,7 +292,7 @@ static const struct panel_desc densitron_dmt101f3nmcmu_1a_panel_desc = {
 	.width_mm = 135,
 	.height_mm = 217,
 	.mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
-		      MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM,
+		      MIPI_DSI_MODE_LPM,
 	.format = MIPI_DSI_FMT_RGB888,
 	.lanes = 4,
 	.on_cmds = densitron_dmt101f3nmcmu_1a_on_cmds,
