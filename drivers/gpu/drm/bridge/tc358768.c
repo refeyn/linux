29f9c205748561ca6a8dbf728136a83a3a2ea1f8
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-10-21 16:23:00 +0300

    drm/bridge: tc358768: Support non-continuous clock
    The driver prints a warning if MIPI_DSI_CLOCK_NON_CONTINUOUS is set, and
    falls back to continuous clock mode. This was added in commit
    fbc5a90e82c1 ("drm/bridge: tc358768: Disable non-continuous clock mode").
    
    However, there have been multiple changes to the driver since then, and
    at least in my setup, non-continuous clock mode works: I can see an
    image on the panel, and I can see the clock lanes being non-continuous
    with an oscilloscope.
    
    So, let's enable MIPI_DSI_CLOCK_NON_CONTINUOUS support.
    
    Upstream-Status: Submitted [https://lore.kernel.org/all/20251021-tc358768-v1-0-d590dc6a1a0c@ideasonboard.com/]
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
    Cc: Dmitry Osipenko <digetx@gmail.com>

--- drivers/gpu/drm/bridge/tc358768.c
+++ drivers/gpu/drm/bridge/tc358768.c
@@ -716,7 +716,6 @@ static void tc358768_bridge_pre_enable(struct drm_bridge *bridge)
 {
 	struct tc358768_priv *priv = bridge_to_tc358768(bridge);
 	struct mipi_dsi_device *dsi_dev = priv->output.dev;
-	unsigned long mode_flags = dsi_dev->mode_flags;
 	u32 val, mask, val2, lptxcnt, hact, data_type;
 	s32 raw_val;
 	const struct drm_display_mode *mode;
@@ -734,11 +733,6 @@ static void tc358768_bridge_pre_enable(struct drm_bridge *bridge)
 	/* In hsbyteclk units */
 	u32 dsi_vsdly;
 	const u32 internal_dly = 40;
-
-	if (mode_flags & MIPI_DSI_CLOCK_NON_CONTINUOUS) {
-		dev_warn_once(dev, "Non-continuous mode unimplemented, falling back to continuous\n");
-		mode_flags &= ~MIPI_DSI_CLOCK_NON_CONTINUOUS;
-	}
 
 	tc358768_hw_enable(priv);
 
@@ -1020,7 +1014,7 @@ static void tc358768_bridge_pre_enable(struct drm_bridge *bridge)
 	tc358768_write(priv, TC358768_HSTXVREGEN, val);
 
 	tc358768_write(priv, TC358768_TXOPTIONCNTRL,
-		       (mode_flags & MIPI_DSI_CLOCK_NON_CONTINUOUS) ? 0 : BIT(0));
+		       (dsi_dev->mode_flags & MIPI_DSI_CLOCK_NON_CONTINUOUS) ? 0 : BIT(0));
 
 	/* TXTAGOCNT[26:16] RXTASURECNT[10:0] */
 	val = tc358768_ps_to_ns((lptxcnt + 1) * hsbyteclk_ps * 4);
@@ -1087,7 +1081,7 @@ static void tc358768_bridge_pre_enable(struct drm_bridge *bridge)
 
 	val |= TC358768_DSI_CONTROL_TXMD;
 
-	if (!(mode_flags & MIPI_DSI_CLOCK_NON_CONTINUOUS))
+	if (!(dsi_dev->mode_flags & MIPI_DSI_CLOCK_NON_CONTINUOUS))
 		val |= TC358768_DSI_CONTROL_HSCKMD;
 
 	if (dsi_dev->mode_flags & MIPI_DSI_MODE_NO_EOT_PACKET)
